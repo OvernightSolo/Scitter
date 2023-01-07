8f821aa9470243b15d65f913b7fa4c19664c00b5
OvernightSolo <sterlingtx75@gmail.com>
2023-01-07 15:17:04 -0600

    add click event function

---
+++ script.js
@@ -0,0 +1,16 @@
+import { tweetsData } from "./data";
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+
+const tweetInput = document.getElementById("tweet-input");
+
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.like) {
+    handleLikeClick(e.target.dataset.like);
+  } else if (e.target.dataset.retweet) {
+    handleRetweetClick(e.target.dataset.retweet);
+  } else if (e.target.dataset.reply) {
+    handleReplyClick(e.target.dataset.reply);
+  } else if (e.target.id === "tweet-btn") {
+    handleTweetBtnClick();
+  }
+});
